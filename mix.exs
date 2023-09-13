b304c5bb8806a882f7847a283e512a5ce9218ce2
Bakhtiar <22100267@lums.edu.pk>
2023-09-13 16:25:45 +0500

    optimize deps

--- mix.exs
+++ mix.exs
@@ -19,7 +19,6 @@ defmodule TodoApp.MixProject do
   def application do
     [
       mod: {TodoApp.Application, []},
-      extra_applications: [:logger, :runtime_tools]
     ]
   end
 
@@ -34,7 +33,7 @@ defmodule TodoApp.MixProject do
     [
       {:phoenix, "~> 1.7.7"},
       {:phoenix_html, "~> 3.3"},
-
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.19.0"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.0"},
@@ -42,6 +41,8 @@ defmodule TodoApp.MixProject do
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:finch, "~> 0.13"},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"}
